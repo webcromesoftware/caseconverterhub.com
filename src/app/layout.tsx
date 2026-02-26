241986720fd1b080b3bcbf19ef817dec36b62f78
webcromesoftware <95916922+webcromesoftware@users.noreply.github.com>
2026-02-26 09:40:43 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,24 @@ export default function RootLayout({
         <SecurityWrapper>
           {children}
         </SecurityWrapper>
+
+
+        {/* Google Analytics */}
+<Script
+  strategy="afterInteractive"
+  src="https://www.googletagmanager.com/gtag/js?id=G-RE62PC2MM5"
+/>
+<Script
+  id="google-analytics"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-RE62PC2MM5');
+    `,
+  }}
       </body>
     </html>
   );
